deedc14bb74590ba96b2ec3bf6d20cc2e60ad95c
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-19 13:18:01 +0900

    Add source

---
+++ src/Timer.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import { Button } from "react-bootstrap";
+const Timer : React.FC = () => {
+    const [seconds, setSeconds] = useState<number>(0);
+
+    return(
+        <div>
+            <h4>타이머 : {seconds} 초</h4>
+            <Button onClick={
+                ()=>{
+                    setInterval(()=>{
+                        setSeconds((prevSeconds)=> {
+                                return prevSeconds + 1
+                            }
+                        );
+                    }, 1000);
+                }
+            }>시작</Button>
+        </div>
+    )
+}
+
+export default Timer;
